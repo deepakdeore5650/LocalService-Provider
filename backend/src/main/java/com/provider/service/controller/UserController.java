fe3da4fabe804f14cbf147004553fe96bf6dc747
deepak deore <deepakdeore5650@gmail.com>
2026-08-02 17:16:13 +0530

    Update UserController.java

--- backend/src/main/java/com/provider/service/controller/UserController.java
+++ backend/src/main/java/com/provider/service/controller/UserController.java
@@ -32,8 +32,7 @@ public class UserController {
         d.setId(e.getId());
         d.setName(e.getName());
         d.setEmail(e.getEmail());
-        d.setRole("ADMIN");
-        // d.setRole(e.getRole());
+        d.setRole(e.getRole());
         d.setServiceType(e.getServiceType());
         d.setPincode(e.getPincode());
         d.setAddress(e.getAddress());
